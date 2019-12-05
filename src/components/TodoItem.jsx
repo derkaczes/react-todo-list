1fb619025a5d4bfe19f934e05ef0d273935338c4
Michał Derkacz <derkaczes@gmail.com>
2019-12-05 18:44:20 +0100

    Modify items display

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#3CFF77',
+            padding: '10px',
+            borderBotton: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+                'line-through' : 'none'
+        }
+    }
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
     }
+}
+
+TodoItem.propsTypes = {
+    todos: PropTypes.object.isRequired
 }
 
 export default TodoItem;
