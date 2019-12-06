859e0349db53ddc42944d92e9ce443190dec46f3
Michał Derkacz <derkaczes@gmail.com>
2019-12-06 15:35:50 +0100

    Create about view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/layout/Header';
 import Todos from './components/Todos.jsx';
 import AddTodo from './components/AddTodo.jsx';
+import About from './components/pages/About.jsx';
 import uuid from 'uuid';
 
 import './App.css';
@@ -52,13 +54,20 @@ class App extends Component {
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About}/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
