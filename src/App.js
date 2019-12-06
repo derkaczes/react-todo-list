358b570d69f9c7d7ccec5cb5f3b467a44576e198
Michał Derkacz <derkaczes@gmail.com>
2019-12-06 14:47:46 +0100

    Modify add funtion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos.jsx';
 import AddTodo from './components/AddTodo.jsx';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with dog',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Playing poker',
         completed: false
       },
@@ -39,13 +40,22 @@ class App extends Component {
     this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]});
+  }
+
   render() {
     console.log(this.state.todos);
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
         </div>
       </div>
